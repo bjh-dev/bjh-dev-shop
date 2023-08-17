a3e9dff3e24986eca6e83e1cfaa97ad7cce13764
Bryan Hickey <bjh@bjh.dev>
2023-08-17 12:16:40 +1000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,36 @@
+import '@/app/globals.css'
+
+// eslint-disable-next-line camelcase
+import { Inter, Roboto_Mono } from 'next/font/google'
+import React from 'react'
+
+if (process.env.NODE_ENV === 'production') {
+  // eslint-disable-next-line no-console, func-names, @typescript-eslint/no-empty-function
+  console.log = function () {}
+}
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
+
+const robotoMono = Roboto_Mono({
+  subsets: ['latin'],
+  variable: '--font-roboto-mono',
+})
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html
+      lang="en"
+      className={`${inter.variable} ${robotoMono.variable} font-sans`}
+      suppressHydrationWarning
+    >
+      <body suppressHydrationWarning>{children}</body>
+    </html>
+  )
+}
---
+++ src/components/pages/home/HomePage.tsx
@@ -0,0 +1,27 @@
+import { LuMail } from 'react-icons/lu'
+
+import ScrollUp from '@/components/ScollUp'
+import { Button } from '@/components/ui/button'
+import { HomePagePayload } from '@/lib/types'
+
+export interface HomePageProps {
+  data: HomePagePayload
+}
+
+function HomePage({ data }: HomePageProps) {
+  const { title } = data
+  console.log(data)
+
+  return (
+    <>
+      <h1 className="text-3xl">{title}</h1>
+      <Button className="mt-4">
+        <LuMail className="mr-2 h-4 w-4" /> Login with Email
+      </Button>
+      {/* Workaround: scroll to top on route change */}
+      <ScrollUp />
+    </>
+  )
+}
+
+export default HomePage
